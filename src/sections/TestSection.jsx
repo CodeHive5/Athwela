04b9e3ef5b812425c6291db3855345a9a97cd887
SasikaSankalana <79664299+SasikaSankalana@users.noreply.github.com>
2023-02-20 18:01:47 +0530

    Added Carousel

---
+++ src/sections/TestSection.jsx
@@ -0,0 +1,104 @@
+import Carousel from "../components/carousel/Carousel";
+import Card from "../components/card/Card";
+
+const TestSection = () => {
+  return (
+    <div
+      style={{
+        maxWidth: 1200,
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: 64,
+      }}
+    >
+      <Carousel show={3}>
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+        <Card
+          imgUrl="https://via.placeholder.com/300x300"
+          imgAlt="test"
+          title="TEST"
+          description="testing 321"
+        />
+      </Carousel>
+    </div>
+  );
+};
+
+export default TestSection;
